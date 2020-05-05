f0483ff26bd9ac02c21ae891a6cf3e00574fcb50
Shinya Sato <kakeradayo@gmail.com>
2020-05-05 16:14:44 +0900

    スマホ対応する試み

--- public/js/Screen.js
+++ public/js/Screen.js
@@ -469,7 +469,7 @@ class Screen
         var x = e.clientX - canvas.offsetLeft;
         var y = e.clientY - canvas.offsetTop - 21;
         if (this.isSmartPhone()) {
-            x = x + 450;
+            this.socket.emit( 'smartPhone-touched' , x, y );
         }
         let c = null;
         let n = null;
