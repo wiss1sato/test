782a5b2d992a861c5abbff6f369edb96741f5a18
Your Name <you@example.com>
2021-01-06 13:06:33 +0900

    a

--- public/js/Screen.js
+++ public/js/Screen.js
@@ -492,6 +492,7 @@ class Screen
             ( p ) =>
             {
                 if (p.strSocketID === this.socket.id) {
+                    console.log(this.isClicked);
                    if (p.playerNum == this.aTeban && !this.isClicked) tebanPlayerFlg = true;
                 }
             } );
